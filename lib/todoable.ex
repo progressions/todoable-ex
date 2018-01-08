0c8c9a5a78dfe2577b17070f3efcfb1bcd206939
Isaac Priestley <progressions@gmail.com>
2018-01-07 21:32:50 -0600

    Client type

--- lib/todoable.ex
+++ lib/todoable.ex
@@ -8,78 +8,86 @@ defmodule Todoable do
   plug Tesla.Middleware.BaseUrl, "http://localhost:4000/api"
   plug Tesla.Middleware.JSON
 
-  def lists(%{token: token}) do
+  defmodule Client do
+    defstruct token: nil,
+              expires_at: nil
+  end
+
+  alias Client
+
+  def lists(%Client{token: token}) do
     %{body: body} = token_auth(token)
     |> get("/lists")
 
     body["lists"]
   end
 
-  def get_list(%{token: token}, id: list_id) do
+  def get_list(%Client{token: token}, id: list_id) do
     %{body: body} = token_auth(token)
     |> get("/lists/#{list_id}")
 
     body
   end
 
-  def create_list(%{token: token}, name: name) do
+  def create_list(%Client{token: token}, name: name) do
     %{body: body} = token_auth(token)
     |> post("/lists", %{list: %{name: name}})
 
     body
   end
 
-  def update_list(%{token: token}, id: list_id, name: name) do
+  def update_list(%Client{token: token}, id: list_id, name: name) do
     %{body: body} = token_auth(token)
     |> patch("/lists/#{list_id}", %{list: %{name: name}})
 
     body
   end
 
-  def delete_list(%{token: token}, id: list_id) do
+  def delete_list(%Client{token: token}, id: list_id) do
     %{body: body} = token_auth(token)
     |> delete("/lists/#{list_id}")
 
     body == ""
   end
 
-  def create_item(%{token: token}, id: list_id, name: name) do
+  def create_item(%Client{token: token}, id: list_id, name: name) do
     %{body: body} = token_auth(token)
     |> post("/lists/#{list_id}/items", %{item: %{name: name}})
 
     body
   end
 
-  def delete_item(%{token: token}, list_id: list_id, item_id: item_id) do
+  def delete_item(%Client{token: token}, list_id: list_id, item_id: item_id) do
     %{body: body} = token_auth(token)
     |> delete("/lists/#{list_id}/items/#{item_id}")
 
     body
   end
 
-  def finish_item(%{token: token}, list_id: list_id, item_id: item_id) do
+  def finish_item(%Client{token: token}, list_id: list_id, item_id: item_id) do
     %{body: body} = token_auth(token)
     |> put("/lists/#{list_id}/items/#{item_id}/finish", %{})
 
     body
   end
 
-  def token_auth(token) do
+  defp token_auth(token) do
     Tesla.build_client([
       {Tesla.Middleware.Headers, %{"Authorization" => "Token token=\"#{token}\""}}
     ])
   end
 
-  def basic_auth(username: username, password: password) do
+  defp basic_auth(username: username, password: password) do
     Tesla.build_client([
       {Tesla.Middleware.BasicAuth, Map.merge(%{username: username, password: password}, %{})}
     ])
     |> post("/authenticate", %{})
   end
 
-  def build_client do
-    %{body: %{"token" => token, "expires_at" => expires_at}} = basic_auth(username: "username", password: "password")
+  def build_client(), do: build_client(username: "username", password: "password")
+  def build_client(username: username, password: password) do
+    %{body: %{"token" => token, "expires_at" => expires_at}} = basic_auth(username: username, password: password)
 
-    %{token: token, expires_at: expires_at}
+    %Client{token: token, expires_at: expires_at}
   end
 end
