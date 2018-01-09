a32a88ddafaa65e143c44ed64cbf3b8930994348
Isaac Priestley <progressions@gmail.com>
2018-01-09 09:43:36 -0600

    Parse JSON in html response and handle base url

--- lib/todoable.ex
+++ lib/todoable.ex
@@ -8,10 +8,16 @@ defmodule Todoable do
   plug Tesla.Middleware.Tuples
   plug Tesla.Middleware.JSON
 
+  @default_base_url "http://localhost:4000/api"
+
   defmodule Client do
     defstruct token: nil,
               expires_at: nil,
               base_url: nil
+  end
+
+  def c do
+    with {:ok, client} <- Todoable.build_client() |> Todoable.authenticate(username: "progressions@gmail.com", password: "todoable", base_url: "http://todoable.teachable.tech/api"), do: client
   end
 
   def lists(%Client{token: token, base_url: base_url}) do
@@ -26,51 +32,51 @@ defmodule Todoable do
     end
   end
 
-  def get_list(%Client{token: token}, id: list_id) do
+  def get_list(%Client{token: token, base_url: base_url}, id: list_id) do
     req(fn () ->
-      token_auth(token)
+      token_auth(token, base_url)
       |> get("/lists/#{list_id}")
     end)
   end
 
-  def create_list(%Client{token: token}, name: name) do
+  def create_list(%Client{token: token, base_url: base_url}, name: name) do
     req(fn () ->
-      token_auth(token)
+      token_auth(token, base_url)
       |> post("/lists", %{list: %{name: name}})
     end)
   end
 
-  def update_list(%Client{token: token}, id: list_id, name: name) do
+  def update_list(%Client{token: token, base_url: base_url}, id: list_id, name: name) do
     req(fn () ->
-      token_auth(token)
+      token_auth(token, base_url)
       |> patch("/lists/#{list_id}", %{list: %{name: name}})
     end)
   end
 
-  def delete_list(%Client{token: token}, id: list_id) do
+  def delete_list(%Client{token: token, base_url: base_url}, id: list_id) do
     req(fn () ->
-      token_auth(token)
+      token_auth(token, base_url)
       |> delete("/lists/#{list_id}")
     end)
   end
 
-  def create_item(%Client{token: token}, list_id: list_id, name: name) do
+  def create_item(%Client{token: token, base_url: base_url}, list_id: list_id, name: name) do
     req(fn () ->
-      token_auth(token)
+      token_auth(token, base_url)
       |> post("/lists/#{list_id}/items", %{item: %{name: name}})
     end)
   end
 
-  def delete_item(%Client{token: token}, list_id: list_id, item_id: item_id) do
+  def delete_item(%Client{token: token, base_url: base_url}, list_id: list_id, item_id: item_id) do
     req(fn () ->
-      token_auth(token)
+      token_auth(token, base_url)
       |> delete("/lists/#{list_id}/items/#{item_id}")
     end)
   end
 
-  def finish_item(%Client{token: token}, list_id: list_id, item_id: item_id) do
+  def finish_item(%Client{token: token, base_url: base_url}, list_id: list_id, item_id: item_id) do
     req(fn () ->
-      token_auth(token)
+      token_auth(token, base_url)
       |> put("/lists/#{list_id}/items/#{item_id}/finish", %{})
     end)
   end
@@ -79,7 +85,7 @@ defmodule Todoable do
     %Client{token: nil, expires_at: nil, base_url: nil}
   end
 
-  def authenticate(client, username: username, password: password), do: authenticate(client, username: username, password: password, base_url: "http://localhost:4000/api")
+  def authenticate(client, username: username, password: password), do: authenticate(client, username: username, password: password, base_url: @default_base_url)
   def authenticate(%Client{token: _token, expires_at: _expires_at}, username: username, password: password, base_url: base_url) do
     basic_auth(username: username, password: password, base_url: base_url)
     |> post("/authenticate", %{})
@@ -93,17 +99,24 @@ defmodule Todoable do
     with {:ok, response}  <- fun.() do
 
       case response.status do
-        code when code in 200..300 -> {:ok, response.body}
+        code when code in 200..300 -> {:ok, parsed_body(response)}
         401 -> {:error, "You are not authenticated."}
         404 -> {:error, "Could not find resource."}
-        _ -> {:error, response.body}
+        _ -> {:error, parsed_body(response)}
       end
     else
       {:error, _} -> {:error, "The server is not available."}
     end
   end
 
-  defp token_auth(token), do: token_auth(token, "http://localhost:4000/api")
+  def parsed_body(response) do
+    case response.headers["content-type"] do
+      "text/html;charset=utf-8" ->
+        with {:ok, body} <- Poison.decode(response.body), do: body
+      _ -> response.body
+    end
+  end
+
   defp token_auth(token, base_url) do
     Tesla.build_client([
       {Tesla.Middleware.BaseUrl, base_url},
@@ -112,11 +125,11 @@ defmodule Todoable do
   end
 
   defp basic_auth(username: username, password: password), do:
-    basic_auth(username: username, password: password, base_url: "http://localhost:4000/api")
+    basic_auth(username: username, password: password, base_url: @default_base_url)
   defp basic_auth(username: username, password: password, base_url: base_url) do
     Tesla.build_client([
       {Tesla.Middleware.BaseUrl, base_url},
-      {Tesla.Middleware.Headers, %{"Accept" => "application/json", "Content-Type" => "application/json"}},
+      {Tesla.Middleware.Headers, %{"Accept" => "application/json", "Content-Type" => @default_base_url}},
       {Tesla.Middleware.BasicAuth, Map.merge(%{username: username, password: password}, %{})},
     ])
   end
