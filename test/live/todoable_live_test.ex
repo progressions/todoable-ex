ef84ca175ad5a318ead332ef8674bfdfcd567aa6
Isaac Priestley <progressions@gmail.com>
2018-01-21 20:34:44 -0600

    Update test

--- test/live/todoable_live_test.ex
+++ test/live/todoable_live_test.ex
@@ -26,71 +26,73 @@ defmodule TodoableBaseUrlTest do
       @password "password"
   end
 
-  setup do
+  test "acts on lists" do
     {:ok, client} =
       Todoable.build_client(base_url: @base_url)
       |> Todoable.authenticate(@username, @password)
 
-    {:ok, client: client}
-  end
-
-  test "acts on lists", state do
-    {:ok, lists} = Todoable.lists(state.client)
+    {:ok, lists} = Todoable.lists(client)
     matches = Enum.filter(lists, fn list -> list.name == "Shopping List" end)
-    Enum.each(matches, fn list -> Todoable.delete_list(state.client, list) end)
+    Enum.each(matches, fn list -> Todoable.delete_list(client, list) end)
 
     # Create list
     #
-    {:ok, list} = Todoable.create_list(state.client, name: "Shopping List")
+    {:ok, list} = Todoable.create_list(client, name: "Shopping List")
     assert list.name == "Shopping List"
     assert list.id != nil
 
     # Check that new list is included in all lists
     #
-    {:ok, lists} = Todoable.lists(state.client)
+    {:ok, lists} = Todoable.lists(client)
     matches = Enum.filter(lists, fn list -> list.name == "Shopping List" end)
     assert length(matches) > 0
 
-    {:ok, list} = Todoable.get_list(state.client, id: list.id)
+    {:ok, list} = Todoable.get_list(client, id: list.id)
+
     Enum.each(list.items, fn item ->
-      Todoable.delete_item(state.client, list_id: list.id, item_id: item.id)
+      Todoable.delete_item(client, list_id: list.id, item_id: item.id)
     end)
 
     # Create an item
     #
-    {:ok, item} = Todoable.create_item(state.client, list_id: list.id, name: "Get some milk")
+    {:ok, item} = Todoable.create_item(client, list_id: list.id, name: "Get some milk")
     assert item.name == "Get some milk"
     assert item.finished_at == nil
+    assert item.list_id == list.id
 
     # Finish an item
     #
     {:ok, "Get some milk finished"} =
-      Todoable.finish_item(state.client, list_id: list.id, item_id: item.id)
+      Todoable.finish_item(client, list_id: list.id, item_id: item.id)
 
     # Get list, check that item exists on it
     #
-    {:ok, list} = Todoable.get_list(state.client, id: list.id)
+    {:ok, list} = Todoable.get_list(client, id: list.id)
     items = Enum.filter(list.items, fn item -> item.name == "Get some milk" end)
     assert length(items) > 0
 
     # Delete item
     #
-    {:ok, ""} = Todoable.delete_item(state.client, list_id: list.id, item_id: item.id)
+    {:ok, ""} = Todoable.delete_item(client, list_id: list.id, item_id: item.id)
 
     # Get list, check that delete item doesn't exist on it
     #
-    {:ok, list} = Todoable.get_list(state.client, id: list.id)
+    {:ok, list} = Todoable.get_list(client, id: list.id)
     items = Enum.filter(list.items, fn item -> item.name == "Get some milk" end)
     assert length(items) == 0
 
     # Delete list
     #
-    assert Todoable.delete_list(state.client, id: list.id) == {:ok, ""}
+    assert Todoable.delete_list(client, id: list.id) == {:ok, ""}
 
     # Check that the deleted list doesn't appear in all lists
     #
-    {:ok, lists} = Todoable.lists(state.client)
+    {:ok, lists} = Todoable.lists(client)
     new_matches = Enum.filter(lists, fn list -> list.name == "Shopping List" end)
     assert length(new_matches) == length(matches) - 1
+
+    # Check that nonexistent list can't be found
+    #
+    {:error, "Could not find resource."} = Todoable.get_list(client, id: list.id)
   end
 end
